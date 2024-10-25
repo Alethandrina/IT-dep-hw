e8899f6a45fab951032bb3c1b2d56805a9f5729e
Alexandra Ogloblina <alexandraogloblina@gmail.com>
2024-10-25 18:07:56 +0300

    corrections

--- HW.go
+++ HW.go
@@ -4,65 +4,77 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 /*
 C:/Users/1/GolandProjects/awesomeProject/ДЗ1/file.txt.txt
-C:/Users/1/GolandProjects/awesomeProject/ДЗ1/5.txt
 */
-func main() {
-	var input_file, output_file string
+func reading() (map[string]int, bool) {
+	var input_file string
 	fmt.Print("Enter your input file: ")
 	fmt.Scan(&input_file)
-	fmt.Print("Enter your output file: ")
-	fmt.Scan(&output_file)
 	file, err := os.Open(input_file)
 	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	s := bufio.NewScanner(file)
-	a := []string{}
-	b := []string{}
-	for s.Scan() {
-		a = append(a, s.Text())
-	}
-	for idx, el := range a {
-		t := true
-		for j := 0; j < len(a); j++ {
-			if j == idx {
-				continue
-			}
-			if el == a[j] {
-				t = false
-				break
+		fmt.Println("Can not find the file", err)
+		dict := make(map[string]int)
+		return dict, false
+	} else {
+		defer file.Close()
+		scaner := bufio.NewScanner(file)
+		dict := make(map[string]int)
+		for scaner.Scan() {
+			line := scaner.Text()
+			kol, ok := dict[line]
+			if ok {
+				dict[line] = kol + 1
+			} else {
+				dict[line] = 1
 			}
 		}
-		if t {
-			b = append(b, el)
+		return dict, true
+	}
+}
+
+func find_uniq(dict map[string]int) []string {
+	a := make([]string, 0, 0)
+	for key, value := range dict {
+		if value == 1 {
+			a = append(a, key)
 		}
 	}
+	return a
+}
+
+func up_sort(b []string) []string {
 	for in, el := range b {
 		b[in] = strings.ToUpper(el)
 	}
-	for i := 0; i < len(b)-1; i++ {
-		imin := i
-		for j := i + 1; j < len(b); j++ {
-			if b[j] < b[imin] {
-				imin = j
-			}
-		}
-		b[i], b[imin] = b[imin], b[i]
-	}
+	sort.Strings(b)
+	return b
+}
+func output(b []string) {
+	var output_file string
+	fmt.Print("Enter your output file: ")
+	fmt.Scan(&output_file)
+
 	file1, err := os.Create(output_file)
 	if err != nil {
-		fmt.Println("Нельзя создать файл", err)
-		os.Exit(1)
+		fmt.Println("Can not create a file", err)
+		return
 	}
 	defer file1.Close()
 	for _, el := range b {
 		file1.WriteString(el + " - " + strconv.Itoa(len(el)) + " байт" + "\n")
+	}
+}
+func main() {
+	a, flag := reading()
+	if flag {
+		b := find_uniq(a)
+		b = up_sort(b)
+		output(b)
 	}
 }
